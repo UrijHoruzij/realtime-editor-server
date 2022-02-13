f7dc6740e5d3cdf99027f8e70201519774236936
Юрий Хоружий <urijhoruzij@gmail.com>
2022-02-13 15:46:45 +0300

    Add buffer

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const WebSocketServer = require('ws').WebSocketServer;
 const wss = new WebSocketServer({ port: 3000 });
 
 console.log('The server is running: 3000');
-
+let buffer = '';
 wss.on('connection', (ws) => {
 	console.log(`Connected`);
+	ws.send(buffer);
 	ws.on('message', (msg) => {
+		buffer = msg.toString();
 		wss.clients.forEach((client) => {
 			if (client.readyState === 1) {
 				client.send(msg.toString());
